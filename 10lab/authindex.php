b643a99a2c6365ba5c87cfad706640745444e312
Sofa <s-yazykova@bk.ru>
2024-12-30 21:37:40 +0700

    курсовая

--- 10lab/authindex.php
+++ 10lab/authindex.php
@@ -1,6 +1,6 @@
 <?php require_once __DIR__ . "/incs/authheader.php"; 
 
-$title = 'Cars';
+$title = 'Books';
 ?>
 <main>
         <section id="banner-section">
@@ -10,12 +10,12 @@ $title = 'Cars';
                     <h2 class="banner__header">РЕЙТИНГ ТОП ЛУЧШИХ КНИГ</h2>
                     <p class="banner__text">МЫ познакомим вас с топом лучших книг на сегодняшний день</p>
                     <div class="banner-links-wrap">
-                    <a href="authbooks.php" class="btn-primary banner__link">КНИГИ</a>
-                    <a href="https://youtu.be/wCemjAwHMK4" target="_blank" class="btn-secondary banner__btn">Смотреть видео</a>
+                        <a href="books.php" class="btn-primary banner__link">КНИГИ</a>
+                        <a href="https://youtu.be/wCemjAwHMK4" target="_blank" class="btn-secondary banner__btn">Смотреть видео</a>
                     </div>
                     <div class="banner-features">
-                        <span class="banner-features__text"><strong class="banner-features__text_strong">500+</strong>RUSSIAN автомобилей</span>
-                        <span class="banner-features__text"><strong class="banner-features__text_strong">#1</strong>Автомобиль по лучшей цене</span>
+                        <span class="banner-features__text"><strong class="banner-features__text_strong">100+</strong>Зарубежных книг и русских</span>
+                        <span class="banner-features__text"><strong class="banner-features__text_strong">#1</strong>Среди рейтинговых платформ</span>
                     </div>
                 </div>
             </div>
